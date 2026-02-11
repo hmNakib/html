21993d5204737bfe4303a5c986f0176ba8c32d3e
hmNakib <hisamnakib29@gmail.com>
2026-02-12 01:57:53 +0600

    saneee

---
+++ creat.js
@@ -0,0 +1,14 @@
+// Simple greeting function
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// Example usage
+console.log(greet("World"));
+
+// Simple calculator
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(5, 3)); // Output: 8
